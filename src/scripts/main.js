60f07c95ff61cfd4cb1ce4548574379d9da2821f
Pavel Kuzmenko <pashokkuzmenko@gmail.com>
2021-04-14 14:21:41 +0300

    added nav scroll

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,9 +2,11 @@
 
 const toggler = document.querySelector('#toggler');
 const nav = document.querySelector('.nav');
+const top = document.querySelector('.header__top');
 
 toggler.addEventListener('click', () => {
   nav.classList.toggle('nav--active');
   toggler.classList.toggle('header__menu--active');
+  top.classList.toggle('header__top--active');
 }
 );
